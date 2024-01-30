a0611c8f3ac0698d84db6affa1296ade25c814f5
Pawel <p.jezierski999@gmail.com>
2024-01-30 02:39:42 +0100

    Made predict async

--- src/predict_rain.py
+++ src/predict_rain.py
@@ -3,7 +3,7 @@ from joblib import load
 import pandas as pd
 
 
-def predict(maksymalna_temperatura, minimalna_temperatura, srednia_temperatura, temperatura_minimalna_przy_gruncie,
+async def predict(maksymalna_temperatura, minimalna_temperatura, srednia_temperatura, temperatura_minimalna_przy_gruncie,
             suma_dobowa_opadow, wysokosc_pokrywy_snieznej):
     loaded_model = load('../model/trained_log_model.joblib')
     dane = {
